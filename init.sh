36ce8d8ccb86600934a5e74cbed100976c3d6401
Nitesh Balusu <niteshbalusu@icloud.com>
2023-04-15 08:24:36 -0400

    add docker installation
    Signed-off-by: Nitesh Balusu <niteshbalusu@icloud.com>

--- init.sh
+++ init.sh
@@ -152,12 +152,19 @@ sudo apt-get install -y \
 print_message "Installing Docker GPG key"
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add Docker repository
+print_message "Adding Docker repository"
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install Docker
 print_message "Installing Docker"
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Verify Docker installation
 print_message "Verifying Docker installation"
